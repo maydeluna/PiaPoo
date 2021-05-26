0e33a14de8a8131f26d53d9d181681ead418f58c
Melissa <melissa.luis.1bprog@gmail.com>
2021-05-26 00:22:29 -0500

    ACTUALIZACION

--- src/Vista/Login.java
+++ src/Vista/Login.java
@@ -20,6 +20,7 @@ public class Login extends javax.swing.JFrame {
     public Login() {
         initComponents();
        this.setLocationRelativeTo(null);
+        System.out.println("ACTUALIZACION");
  
     }
     
